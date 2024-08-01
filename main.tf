58b6f88e7c281d70d59b3f6d1a595f8884bef95a
VaishnaviDeshmukh2002 <145598486+VaishnaviDeshmukh2002@users.noreply.github.com>
2024-08-01 23:56:23 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "vaish" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
 }
-resource "aws_instance" "vaish" {
+resource "aws_instance" "vaish2" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
 }
