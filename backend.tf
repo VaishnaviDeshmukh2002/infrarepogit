bd805baae81f9e4aa0adc89c4506ea01bf00cf68
VaishnaviDeshmukh2002 <145598486+VaishnaviDeshmukh2002@users.noreply.github.com>
2024-08-05 14:03:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "bucket-statefile"
-    key            = "path/to/my/key"
+    key            = "terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "vaish-statefilelock"
     encrypt        = true
   }
 }
