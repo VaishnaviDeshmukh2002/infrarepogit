1d627f672d70fc948b74cd1a4a4d67d4bd0f3da7
VaishnaviDeshmukh2002 <145598486+VaishnaviDeshmukh2002@users.noreply.github.com>
2024-08-05 13:27:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
     encrypt        = true
   }
 }
-resource "aws_instance" "infra20" {
+resource "aws_instance" "infra2" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
 }
