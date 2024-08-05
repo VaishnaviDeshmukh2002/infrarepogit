ed5024f6172399e7736edc024e2bcd189b5f99d7
VaishnaviDeshmukh2002 <145598486+VaishnaviDeshmukh2002@users.noreply.github.com>
2024-08-05 14:08:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,34 +2,3 @@ resource "aws_instance" "infra2" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
 }
-
-resource "aws_instance" "infra21" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
-resource "aws_instance" "infra22" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
-resource "aws_instance" "infra23" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
-resource "aws_instance" "infra24" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
-resource "aws_instance" "infra25" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
-resource "aws_instance" "infra26" {
- ami           = "ami-0e97ea97a2f374e3d" 
- instance_type = "t2.micro"
-}
-
