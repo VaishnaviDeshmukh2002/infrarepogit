8a0836c3231ea8c9466109ae641c97d966fe2fb7
VaishnaviDeshmukh2002 <145598486+VaishnaviDeshmukh2002@users.noreply.github.com>
2024-08-05 14:18:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "aws_instance" "infra21" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
 }
+resource "aws_instance" "infra21" {
+ ami           = "ami-0e97ea97a2f374e3d" 
+ instance_type = "t2.micro"
+}
