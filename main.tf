07667c8ae736a59682ab8216b6c5728eb8c17726
vaishnavi <vaishnavi.deshmukh@blazeclan.com>
2024-08-05 13:12:28 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "bucket-statefile"
+    key            = "path/to/my/key"
+    region         = "us-east-1"
+    dynamodb_table = "vaish-statefilelock"
+    encrypt        = true
+  }
+}
+
 resource "aws_instance" "infra20" {
  ami           = "ami-0e97ea97a2f374e3d" 
  instance_type = "t2.micro"
